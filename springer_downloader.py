010e53f4b36e8b2f9ea83ced4e98a37401047bfa
Luis Cardozo <me@luiscardozo.dev>
2020-05-04 21:06:30 -0400

    refactor: Change *downloader* to a Class
    To be able to encapsulate better the options required to run.
    Add also argparse to parse the command line options.

--- springer_downloader.py
+++ springer_downloader.py
@@ -1,6 +1,28 @@
-from springer_free_downloader import downloader
+import os
+from argparse import ArgumentParser
+
+from springer_free_downloader.downloader import Downloader
+
+DEFAULT_CSV = os.path.dirname(__file__) + '/lists/list_ebooks.csv'
+DEFAULT_DIR = os.path.expanduser('~') + os.path.sep + 'SpringerBooks'
+
+def build_argparser():
+    """
+    Parse command line arguments.
+
+    :return: command line arguments
+    """
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--download_dir", required=False, type=str, default=DEFAULT_DIR, 
+                        help="Directory where the e-books will be downloaded")
+    parser.add_argument("-c", "--csv", required=False, type=str, default=DEFAULT_CSV,
+                        help="Path to alternative CSV file")
+    return parser
+
 
 def main():
+    args = build_argparser().parse_args()
+    downloader = Downloader(args.download_dir, args.csv)
     downloader.from_list()
 
 if __name__ == '__main__':
--- springer_free_downloader/downloader.py
+++ springer_free_downloader/downloader.py
@@ -5,84 +5,89 @@ import requests
 import bs4
 import pandas as pd
 
-DEFAULT_CSV = os.path.dirname(__file__) + '/lists/list_ebooks.csv'
-DEFAULT_DIR = os.path.expanduser('~') + os.path.sep + 'SpringerBooks'
-
 BASE_URL = 'https://link.springer.com'
 PDF_CLASS='test-bookpdf-link'
 EBOOK_CLASS='test-bookepub-link'
 
+class Downloader():
 
-def download_book(url, dirname=DEFAULT_DIR):
-    """
-    Download specific book (PDF or ePub) in *dirname*
-    """
-    os.makedirs(dirname, exist_ok=True)
+    def __init__(self, download_dir, csv_file):
+        self._download_dir = download_dir
+        self._csv_file = csv_file
 
-    book_url = BASE_URL + url
-    print('Book URL: ', book_url)
-
-    res = requests.get(book_url)
-    res.raise_for_status()
-
-    try:
-        file_name = os.path.join(dirname,res.headers['content-disposition'].split('=')[1])
-    except:
-        file_name = os.path.join(dirname, os.path.basename(url))
-
-    print(f"Guardando a {file_name}")
-
-    with open(file_name,'wb') as book_file:
-        for chunk in res.iter_content(100000):
-            book_file.write(chunk)
+        os.makedirs(self._download_dir , exist_ok=True)
     
+    def download_book(self, url):
+        """
+        Download specific book (PDF or ePub) in *dirname*
+        """
+        book_url = BASE_URL + url
+        print('Book URL: ', book_url)
 
-def get_ebook_url(soup, booktype='pdf'):
-    """
-    Select the link for the booktype (pdf or ebook) and downloads it.
-    """
+        res = requests.get(book_url)
+        res.raise_for_status()
 
-    if booktype == 'pdf':
-        klass = PDF_CLASS
-    else:
-        klass = EBOOK_CLASS
+        try:
+            #Try to get the filename from the HTTP headers
+            file_name = os.path.join(self._download_dir,
+                                     res.headers['content-disposition'].split('=')[1])
+        except:
+            # if not, name it as the URL
+            file_name = os.path.join(self._download_dir, os.path.basename(url))
 
-    downBook = soup.select(f'a.{klass}')
-    if downBook == []:
-        print(f'It seems to be no {booktype} book to download')
-    else:
-        ref = downBook[0].get('href')
-        download_book(ref)
+        print(f"Saving to {file_name}")
 
-def parse_page(url):
-    """
-    Searchs for links to download the eBooks (PDF and ePub)
-    """
-    doc = requests.get(url)
-    doc.raise_for_status()
+        with open(file_name,'wb') as book_file:
+            for chunk in res.iter_content(100000):
+                book_file.write(chunk)
+        
 
-    soup = bs4.BeautifulSoup(doc.text, features="html.parser")
-    
-    get_ebook_url(soup)
-    get_ebook_url(soup, 'ebook')
+    def get_ebook_url(self, soup, booktype='pdf'):
+        """
+        Select the link for the booktype (pdf or ebook) and downloads it.
+        """
 
-def from_list(csv=DEFAULT_CSV):
-    """
-    Reads the list of eBook URLs from CSV file and downloads
-    them all.
-    """
-    df = pd.read_csv(csv)
+        if booktype == 'pdf':
+            klass = PDF_CLASS
+        else:
+            klass = EBOOK_CLASS
 
-    url_size = df['OpenURL'].size
-    file_nr = 0
+        downBook = soup.select(f'a.{klass}')
+        if downBook == []:
+            print(f'It seems to be no {booktype} book to download')
+        else:
+            ref = downBook[0].get('href')
+            download_book(ref)
 
-    start = time.perf_counter()
+    def parse_page(self, url):
+        """
+        Searchs for links to download the eBooks (PDF and ePub)
+        """
+        doc = requests.get(url)
+        doc.raise_for_status()
 
-    for url in df['OpenURL']:
-        file_nr += 1
+        soup = bs4.BeautifulSoup(doc.text, features="html.parser")
+        
+        get_ebook_url(soup)
+        get_ebook_url(soup, 'ebook')
 
-        print(f'Downloading file #{file_nr} of {url_size}')
-        parse_page(url)
-    
-    end = time.perf_counter()
-    print(f"downloaded {url_size} files in {end - start} seconds")
+    def from_list(self):
+        """
+        Reads the list of eBook URLs from CSV file and downloads
+        them all.
+        """
+        df = pd.read_csv(self._csv_file)
+
+        url_size = df['OpenURL'].size
+        file_nr = 0
+
+        start = time.perf_counter()
+
+        for url in df['OpenURL']:
+            file_nr += 1
+
+            print(f'Downloading file #{file_nr} of {url_size}')
+            parse_page(url)
+        
+        end = time.perf_counter()
+        print(f"downloaded {url_size} files in {end - start} seconds")
